83a1146bc20ede45aadfd03acaa7519023405723
kipariss1 <kpassudin@gmail.com>
2024-06-27 22:24:15 +0200

    basic variadic arguments works

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: krassudi <krassudi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:36:00 by krassudi          #+#    #+#             */
-/*   Updated: 2024/06/24 21:52:01 by krassudi         ###   ########.fr       */
+/*   Updated: 2024/06/27 22:17:02 by krassudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,16 @@ int	ft_printf(const char *str, ...)
 	va_list	args;
 	
 	i = 0;
+	ft_putstr_fd((char *)str, 1);
+	ft_putchar_fd('\n', 1);
 	va_start(args, str);
-	printf("%s", str);
+	while (i < 3)
+	{
+		int arg = va_arg(args, int);
+		ft_putnbr_fd(arg, 1);
+		i++;
+	}
+	ft_putchar_fd('\n', 1);
 	va_end(args);
 	(void)i;
 	return (0);
@@ -28,6 +36,6 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	char test = 'a';
-	ft_printf("%c", test);
+	ft_printf("This is the test for printing numbers: %d %d %d", 1, 2, 3);
+	return (0);
 }
